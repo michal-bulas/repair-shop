5b26635734193ebba01eaf5179f76546b320e1fd
michal-bulas <michal.bulas1@gmail.com>
2024-11-26 17:59:33 +0100

    Implement SQL database using Neon, Drizzle, PostreSQL and insert dummy data

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,15 +8,15 @@ export const config = {
   matcher: [
     /*
      * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
+     * - API routes
+     * - _next/static
+     * - _next/image
      * - auth
-     * - favicon.ico (favicon file)
+     * - favicon.ico
      * - robots.txt
      * - images
      * - login
-     * - homepage (represented with $ after beginning /)
+     * - homepage
      */
     '/((?!api|_next/static|_next/image|auth|favicon.ico|robots.txt|images|login|$).*)'
   ]
